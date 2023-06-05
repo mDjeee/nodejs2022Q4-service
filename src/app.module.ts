ee6a8a1eb9934d331140636283e61e34fa4d9b86
Islambek Seytniyazov <islambek_seytniyazov@epam.com>
2023-06-05 22:59:08 +0500

    feat: generate favs

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UsersModule } from './users/users.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { TrackModule } from './track/track.module';
+import { FavsModule } from './favs/favs.module';
 
 @Module({
-  imports: [UsersModule, ArtistModule, AlbumModule, TrackModule],
+  imports: [UsersModule, ArtistModule, AlbumModule, TrackModule, FavsModule],
   controllers: [AppController],
   providers: [AppService],
 })
