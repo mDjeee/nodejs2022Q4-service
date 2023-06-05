87a393a1173422100b0266ced1831658af637b98
Islambek Seytniyazov <islambek_seytniyazov@epam.com>
2023-06-05 18:30:18 +0500

    fix: fix validation pipe

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
-
   app.useGlobalPipes(new ValidationPipe());
+  await app.listen(4000);
 }
 bootstrap();
